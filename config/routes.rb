2a0c736f12505c135805a4c14d23019d6fb8605c
tnummy <tim.nummy@luceous.com>
2015-02-25 19:19:26 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   get 'notig/profile'
 
-  get 'not_ig/home'
-
-  get 'not_ig/profile'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
